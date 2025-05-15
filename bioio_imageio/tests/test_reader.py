7131c1f6ae9422048404c86cee185ba5c9a5d141
DMT <danielt@alleninstitute.org>
2025-05-15 11:22:39 -0700

    update the gif test case

--- bioio_imageio/tests/test_reader.py
+++ bioio_imageio/tests/test_reader.py
@@ -18,7 +18,7 @@ from .conftest import LOCAL_RESOURCES_DIR
         ("example.bmp", "Image:0", (480, 640, 3), "YXS"),
         ("example.png", "Image:0", (800, 537, 4), "YXS"),
         ("example.jpg", "Image:0", (452, 400, 3), "YXS"),
-        ("example.gif", "Image:0", (72, 268, 268, 3), "TYXS"),
+        ("example.gif", "Image:0", (72, 268, 268, 4), "TYXS"),
         (
             "example_invalid_frame_count.mp4",
             "Image:0",
